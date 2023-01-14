72ac917202ce0c2f02a08b71e5660b56791705e9
kaltag <alexx30rus@gmail.com>
2023-01-14 11:51:04 +0400

    add rubocop + int tests

---
+++ spec/features/endpoint_tests_spec.rb
@@ -0,0 +1,45 @@
+require "rails_helper"
+
+feature "Attendance", type: :feature, js: true do
+
+  scenario "get current temperature" do
+    VCR.use_cassette 'weather_info' do
+      visit weather_current_path
+      expect(page).to have_content("-6.1")
+    end
+  end
+
+  scenario "get 24h temperature" do
+    VCR.use_cassette 'weather_info_history' do
+      visit weather_historical_path
+      expect(page).to have_content("-5.0")
+      expect(page).to have_content("-7.2")
+      expect(page).to have_content("-7.8")
+      expect(page).to have_content("-6.1")
+      expect(page).to have_content("-3.9")
+      expect(page).to have_content("-2.8")
+    end
+  end
+
+  scenario "get max temperature" do
+    VCR.use_cassette 'weather_info_history' do
+      visit weather_historical_max_path
+      expect(page).to have_content("-2.8")
+    end
+  end
+
+  scenario "get min temperature" do
+    VCR.use_cassette 'weather_info_history' do
+      visit weather_historical_min_path
+      expect(page).to have_content("-7.8")
+    end
+  end
+
+  scenario "get avg temperature" do
+    VCR.use_cassette 'weather_info_history' do
+      visit weather_historical_avg_path
+      expect(page).to have_content("-5.925")
+    end
+  end
+
+end
---
+++ spec/support/chrome.rb
@@ -0,0 +1,43 @@
+options = Selenium::WebDriver::Chrome::Options.new
+options.add_preference(:download, prompt_for_download: false, default_directory: '/tmp/downloads')
+options.add_preference(:browser, set_download_behavior: { behavior: 'allow' })
+options.add_argument('--window-size=1400,900')
+
+Capybara.register_driver :chrome do |app|
+  Capybara::Selenium::Driver.new(app, browser: :chrome, capabilities: [options])
+end
+
+Capybara.register_driver :headless_chrome do |app|
+  options.add_argument('--headless')
+  options.add_argument('--disable-gpu')
+  options.add_argument('--no-sandbox')
+
+  driver = Capybara::Selenium::Driver.new(app, browser: :chrome, capabilities: [options])
+
+  ### Allow file downloads in Google Chrome when headless!!!
+  ### https://bugs.chromium.org/p/chromium/issues/detail?id=696481#c89
+  bridge = driver.browser.send(:bridge)
+
+  path = '/session/:session_id/chromium/send_command'
+  path[':session_id'] = bridge.session_id
+
+  bridge.http.call(
+    :post,
+    path,
+    cmd: 'Page.setDownloadBehavior',
+    params: {
+      behavior: 'allow',
+      downloadPath: '/tmp/downloads'
+    }
+  )
+
+  driver
+end
+
+if ENV["HEADLESS_CHROME"]
+  Capybara.default_driver = :headless_chrome
+  Capybara.javascript_driver = :headless_chrome
+else
+  Capybara.default_driver = :chrome
+  Capybara.javascript_driver = :chrome
+end
